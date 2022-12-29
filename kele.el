83d3247ac4378ed07504aba7f86ec765b9daaa75
Jonathan Jin <me@jonathanj.in>
2022-12-29 11:58:25 -0500

    Implement initial functionality

--- kele.el
+++ kele.el
@@ -7,7 +7,7 @@
 ;; Version: 0.0.1
 ;; Homepage: https://github.com/jinnovation/kele.el
 ;; Keywords: kubernetes
-;; Package-Requires: ((emacs "27.1"))
+;; Package-Requires: ((emacs "27.1") (f "0.20.0"))
 
 ;;; Commentary:
 
@@ -18,6 +18,57 @@
 ;; For more details, see: https://github.com/jinnovation/kele.el.
 
 ;;; Code:
+
+(require 'f)
+
+(defgroup kele nil
+  "Integration constructs for Kubernetes."
+  :group 'external
+  :prefix "kele-"
+  :link '(url-link "https://github.com/jinnovation/kele.el"))
+
+(defcustom kele-kubeconfig-path
+  (expand-file-name (or (getenv "KUBECONFIG") "~/.kube/config"))
+  "Path to the kubeconfig file."
+  :type 'file)
+
+(defvar kele-current-context nil
+  "The current kubectl context.
+
+The value is kept up-to-date with any changes to the underlying
+configuration, e.g. via `kubectl config'.")
+
+(defvar kele-current-namespace nil
+  "The current kubectl namespace.
+
+The value is kept up-to-date with any changes to the underlying
+configuration, e.g. via `kubectl config'.")
+
+(defun kele--update (&optional _)
+  "Update `kele-k8s-context' and `kele-k8s-namespace'.
+
+Values are parsed from the contents at `kele-kubeconfig-path'."
+  (when-let* ((config (yaml-parse-string (f-read kele-kubeconfig-path)))
+              (current-context (ht-get config 'current-context))
+              (contexts (-concat (ht-get config 'contexts) '()))
+              (context (-first (lambda (elem) (string= (ht-get elem 'name) current-context)) contexts)))
+    (let ((namespace (ht-get* context 'context 'namespace)))
+      (setq kele-current-context current-context
+            kele-current-namespace namespace)))))
+
+(defvar kele--kubeconfig-watcher nil
+"Descriptor of the file watcher on `kele-kubeconfig-path'.")
+
+(define-minor-mode kele-mode
+"Minor mode to enable listening on Kubernetes configs."
+:global t
+:group 'kele
+:lighter nil
+(if kele-mode
+    (file-notify-rm-watch kele--kubeconfig-watcher)
+  (setq kele--kubeconfig-watcher
+        (file-notify-add-watch kele-kubeconfig-path '(change) 'kele--update))
+  (kele--update)))
 
 (provide 'kele)
 
