51d37a0eda76bbea8c7674707272f9abee9f25af
Jonathan Jin <me@jonathanj.in>
2022-12-29 10:49:46 -0500

    Initial pkg scaffolding

---
+++ kele.el
@@ -0,0 +1,24 @@
+;;; kele.el --- Kubernetes Enablement Layer for Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022-2022 Jonathan Jin
+
+;; Author: Jonathan Jin <me@jonathanj.in>
+
+;; Version: 0.0.1
+;; Homepage: https://github.com/jinnovation/kele.el
+;; Keywords: kubernetes
+;; Package-Requires: ((emacs "27.1"))
+
+;;; Commentary:
+
+;; kele.el is a streamlines integration between Kubernetes and Emacs.  It
+;; provides a "base layer" that can be leveraged to build higher-level
+;; integrations, e.g. modeline modules and interactive clients.
+;;
+;; For more details, see: https://github.com/jinnovation/kele.el.
+
+;;; Code:
+
+(provide 'kele)
+
+;;; kele.el ends here
