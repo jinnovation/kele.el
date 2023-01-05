6f78ea088e4652e4260d9183d844618bcac1ad1f
Jonathan Jin <me@jonathanj.in>
2023-01-04 23:11:12 -0500

    add simple test for proxy process creation

--- tests/integration/test-integration.el
+++ tests/integration/test-integration.el
@@ -3,7 +3,7 @@
 (require 'async)
 (require 'f)
 (require 'kele)
-(require 'buttercup)
+(require 'plz)
 
 (describe "config interactions"
   (describe "kele-namespace-switch-for-context"
@@ -12,3 +12,8 @@
       (kele-namespace-switch-for-context "kind-kele-test-cluster0" "kube-public")
       (async-wait (kele--update-kubeconfig))
       (expect (kele-current-namespace) :to-equal "kube-public"))))
+
+(describe "kele--proxy-process"
+  (it "successfully creates a proxy process"
+    (kele--proxy-process "kind-kele-test-cluster0" 9999 :wait t :read-only t)
+    (expect (plz 'get "http://127.0.0.1:9999/readyz") :to-equal "ok")))
