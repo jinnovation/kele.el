9c8335c16de89292a2b6d0df10a569862ebd9c75
Jonathan Jin <me@jonathanj.in>
2023-01-24 13:10:05 -0500

    add a test on `g` binding

--- tests/integration/test-ui.el
+++ tests/integration/test-ui.el
@@ -13,18 +13,33 @@
   (describe "kele-resource"
 
     (before-each
+      (spy-on 'kele-get)
+
       (async-wait (kele--cache-update kele--global-discovery-cache))
-      (async-wait (kele--cache-update kele--global-kubeconfig-cache))
-      (with-simulated-input
-       "deployments RET"
-       (call-interactively #'kele-resource)))
+      (async-wait (kele--cache-update kele--global-kubeconfig-cache)))
 
-    (it "sets the current context as default value"
-      (with-current-buffer transient--buffer-name
-        (expect (buffer-string) :to-match "--context=kind-kele-test-cluster0")))
+    (describe "prefix buffer contents"
+      (before-each
+        (with-simulated-input
+         "deployments RET"
+         (call-interactively #'kele-resource)))
 
-    (it "sets the current context's default namespace as the default value"
-      (expect (buffer-string) :to-match "--namespace=kube-public"))))
+      (it "sets the current context as default value"
+        (with-current-buffer transient--buffer-name
+          (expect (buffer-string) :to-match "--context=kind-kele-test-cluster0")))
+      (it "sets the current context's default namespace as the default value"
+        (expect (buffer-string) :to-match "--namespace=kube-public")))
+
+    (describe "get"
+      (it "retrieves with the appropriate parameters"
+        (with-simulated-input
+         "deployments RET g coredns RET"
+         (expect 'kele-get :to-have-been-called-with
+                 "deployments" "coredns"
+                 :group "apps"
+                 :version "v1"
+                 :namespace "kube-public"
+                 :context "kind-kele-test-cluster0"))))))
 
 ;;; test-ui.el ends here
 
